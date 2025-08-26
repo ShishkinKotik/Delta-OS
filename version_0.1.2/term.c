a11231b79959d9012d61a08c18ea4fd6b8810398
Nya <votinovilya174@gmail.com>
2025-08-26 10:29:17 +0500

    Update term.c

--- version_0.1.2/term.c
+++ version_0.1.2/term.c
@@ -19,32 +19,53 @@
 #define MAX_LINES 512
 #define MAX_LINE_LENGTH 1024
 
+void LoadScreen()
+{
+    printf(T_BLUE "[Start loading OS]\n" T_RESET);
+    printf(T_GREEN "[OK]: Compiling term.c\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading commands\n" T_RESET);
+    printf(T_GREEN "[OK]: Commands is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command help\n" T_RESET);
+    printf(T_GREEN "[OK]: Command help is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command calc\n" T_RESET);
+    printf(T_GREEN "[OK]: Command calc is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command edit\n" T_RESET);
+    printf(T_GREEN "[OK]: Command edit is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command phnxfetch\n" T_RESET);
+    printf(T_GREEN "[OK]: Command phnxfetch is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command ext\n" T_RESET);
+    printf(T_GREEN "[OK]: Command ext is load\n");
+    printf(T_GREEN "[OK]: Initialization working all commands\n" T_RESET);
+    printf(T_GREEN "[OK]: Optimization working all ocmmands\n" T_RESET);
+    printf(T_BLUE "[Loading is end!]\n" T_RESET);
+}
+
 void printLogo()
 {
-    printf(T_YELLOW "  ______                                                 \n" T_RESET);
-    printf(T_YELLOW " ||    ||                         _____       ________// \n" T_RESET);
-    printf(T_YELLOW " ||____||                       //     XX     XX         \n" T_RESET);
-    printf(T_YELLOW " ||                            //       XX     XX        \n" T_RESET);
-    printf(T_YELLOW " ||  ||      ||XX  || XX  //  //         XX     XX___    \n" T_RESET);
-    printf(T_YELLOW " ||  ||==||  || XX ||  XX//   XX         //        //    \n" T_RESET);
-    printf(T_YELLOW " ||  ||  ||  ||  XX||  //XX    XX       //        //     \n" T_RESET);
-    printf(T_YELLOW " ||  ||  ||  ||    || //  XX    XX_____//   XX___//      \n" T_RESET);
+    printf(T_YELLOW "____________                                                             \n" T_RESET);
+    printf(T_YELLOW "||        XX                ||===||                                        _____         ________   \n" T_RESET);
+    printf(T_YELLOW "||         XX               ||   ||        ||       //===XX  ||          //     XX      XX     ||   \n" T_RESET);
+    printf(T_YELLOW "||          XX   //===XX         ||     ===||===   //     XX ||         //       XX      XX         \n" T_RESET);
+    printf(T_YELLOW "||           XX //     XX        ||        ||     //       XX||        //         XX      XX____    \n" T_RESET);
+    printf(T_YELLOW "||          //  XX_____XX        ||        ||      XX      //||        XX         //          //    \n" T_RESET);
+    printf(T_YELLOW "||         //    XX              ||        ||       XX    // ||   XX    XX       //  XX      //     \n" T_RESET);
+    printf(T_YELLOW "|_________//      XX__//   ||____||____||   XX===__  XX__//  ||__XX      XX_____//    XX____//       \n" T_RESET);
 }
 
 void printInfoAboutUser()
 {
     printf(T_YELLOW "____________________________ \n" T_RESET);
-    printf(T_YELLOW "phoenixOSuser@PhoenixOS~$ \n" T_RESET);
+    printf(T_YELLOW "deltaOSuser@DeltaOS~$ \n" T_RESET);
     printf(T_YELLOW "---------------------------- \n" T_RESET);
     printf(T_YELLOW "[OS]: [PhoenixOS v 1.0.2] \n" T_RESET);
-    printf(T_YELLOW "[Host]: [HP Laptop 15-bs0xx] \n" T_RESET); //Host может быть каким угодно здесь представлен именно мой хост
+    printf(T_YELLOW "[Host]: [Delta Laptop 15-jd0xxs] \n" T_RESET);
     printf(T_YELLOW "[Kernel]: [6.8.0-71-generic] \n" T_RESET);
-    printf(T_YELLOW "[Packages]: [0] \n" T_RESET);
+    printf(T_YELLOW "[Packages]: [1(fnxp)] \n" T_RESET);
     printf(T_YELLOW "[Shell]: [bash] \n" T_RESET);
-    printf(T_YELLOW "[Theme]: [dark-with-blur] \n" T_RESET);//здесь тоже самое что и выше
+    printf(T_YELLOW "[Theme]: [dark-with-blur] \n" T_RESET);
     printf(T_YELLOW "[Terminal]: [phnxOS-termianl] \n" T_RESET);
-    printf(T_YELLOW "[CPU]: [Intel core i5 7th gen] \n" T_RESET);//Процыки тоже могут быть разными просто указал своё
-    printf(T_YELLOW "[GPU]: [Radeon Graphics] \n" T_RESET);//здесь также
+    printf(T_YELLOW "[CPU]: [Gamma 1 7th gen] \n" T_RESET);
+    printf(T_YELLOW "[GPU]: [Gamma GPU] \n" T_RESET);
 }
 
 void welcome()
@@ -52,7 +73,7 @@ void welcome()
     const char frstln[] = ".____________________________.";
     const char newlin[] = "|                            |";
     const char scndln[] = "|FFFFFFFFFFFFFFFFFFFFFFFFFFFF|";
-    const char thrdln[] = "|[ Welcome to the PhoenixOS!]|";
+    const char thrdln[] = "|[ Welcome to the DeltaOS!  ]|";
     const char frthln[] = "|FFFFFFFFFFFFFFFFFFFFFFFFFFFF|";
     const char endlne[] = "|____________________________|";
 
@@ -62,12 +83,71 @@ void welcome()
     printf("%s\n", endlne);
 }
 
+void edit()
+{
+    printf(T_BLUE "[текстовый редактор]\n" T_RESET);
+    char lines[MAX_LINES][MAX_LINE_LENGTH];
+    int lineCount = 0;
+    char inputLine[MAX_LINE_LENGTH];
+    bool emptyLineEntered = false;
+
+    printf(T_CYAN "[начните ввод(введите 'x' для выхода)]: \n" T_RESET);
+
+    getchar();
+
+    while (lineCount < MAX_LINES) {
+        printf("%d: ", lineCount + 1);
+        if (fgets(inputLine, sizeof(inputLine), stdin) == NULL) {
+            fprintf(stderr, T_RED "[err]: [ошибка чтения строки.]\n" T_RESET);
+            break;
+        }
+
+        inputLine[strcspn(inputLine, "\n")] = 0;
+
+        if (strlen(inputLine) == 0) {
+            break;
+        }
+
+        strcpy(lines[lineCount], inputLine);
+        lineCount++;
+
+        if(lineCount >= MAX_LINES) {
+            printf(T_YELLOW "[warn]: [достигнуто максимальное количество строк. Завершите ввод\n" T_RESET);
+            break;
+        }
+
+        if (strcmp(inputLine, "x") == 0) {
+            emptyLineEntered = true;
+            break;
+        }
+
+    }
+
+    printf(T_CYAN "[Редактор завершил работу]\n" T_RESET);
+}
+
+void help()
+{
+    const char calc[250] = "1.calc - калькулятор";
+    const char edit[250] = "2.dedit - примитивный текстовый редактор";
+    const char help[250] = "3.help - вывод всех команд";
+    const char ftch[250] = "4.dltfetch - выводит логотип ОС а также информация о компьютере и ос";
+    const char exit[250] = "5.ext - выход и завершение работы терминала";
+
+    printf("%s\n", calc);
+    printf("%s\n", edit);
+    printf("%s\n", help);
+    printf("%s\n", ftch);
+    printf("%s\n", exit);
+}
+
 int main()
 {
+    LoadScreen();
     welcome();
     char command[256];
     while (true) {
-        printf("phoenixOSuser$~: ");
+        printf("deltaOSuser$~: ");
         if (scanf("%255s", command) != 1) {
             fprintf(stderr, T_RED "[err]: [ошибка ввода команды!]\n" T_RESET);
         }
@@ -117,65 +197,17 @@ int main()
             printf(T_CYAN "[калькулятор завершил работу]\n" T_RESET);
         }
 
-        if (strcmp(command, "phedx") == 0) {
-            printf(T_BLUE "[текстовый редактор]\n" T_RESET);
-            char lines[MAX_LINES][MAX_LINE_LENGTH];
-            int lineCount = 0;
-            char inputLine[MAX_LINE_LENGTH];
-            bool emptyLineEntered = false;
-
-            printf(T_CYAN "[начните ввод(введите 'x' для выхода)]: \n" T_RESET);
-
-            getchar();
-
-            while (lineCount < MAX_LINES) {
-                printf("%d: ", lineCount + 1);
-                if (fgets(inputLine, sizeof(inputLine), stdin) == NULL) {
-                    fprintf(stderr, T_RED "[err]: [ошибка чтения строки.]\n" T_RESET);
-                    break;
-                }
-
-                inputLine[strcspn(inputLine, "\n")] = 0;
-
-                if (strlen(inputLine) == 0) {
-                    break;
-                }
-
-                strcpy(lines[lineCount], inputLine);
-                lineCount++;
-
-                if(lineCount >= MAX_LINES) {
-                    printf(T_YELLOW "[warn]: [достигнуто максимальное количество строк. Завершите ввод\n" T_RESET);
-                    break;
-                }
-
-                if (strcmp(inputLine, "x") == 0) {
-                    emptyLineEntered = true;
-                    break;
-                }
-
-            }
-
-            printf(T_CYAN "[Редактор завершил работу]\n" T_RESET);
+        if (strcmp(command, "dedit") == 0) {
+            edit();
         }
 
-        if (strcmp(command, "phnxfetch") == 0) {
+        if (strcmp(command, "dltfetch") == 0) {
             printLogo();
             printInfoAboutUser();
         }
 
         if (strcmp(command, "help") == 0) {
-            const char calc[250] = "1.calc - калькулятор";
-            const char edit[250] = "2.phedx - примитивный текстовый редактор";
-            const char help[250] = "3.help - вывод всех команд";
-            const char ftch[250] = "4.phnxfetch - выводит логотип ОС а также информация о компьютере и ос";
-            const char exit[250] = "5.ext - выход и завершение работы терминала";
-
-            printf("%s\n", calc);
-            printf("%s\n", edit);
-            printf("%s\n", help);
-            printf("%s\n", ftch);
-            printf("%s\n", exit);
+            help();
         }
 
         if (strcmp(command, "ext") == 0) {
