8845e614796b9b387ff74931ae1fb0e91c14e466
Dltp <votinovilya174@gmail.com>
2025-08-29 11:56:12 +0500

    Create commands.h

---
+++ version_0.1.6/headers/commands.h
@@ -0,0 +1,21 @@
+#ifndef EDIT_H
+#define EDIT_H
+
+void edit();
+
+#endif 
+
+#ifndef FETCH_H
+#define FETCH_H
+
+void printLogo();
+void printInfoAboutUser();
+
+#endif
+
+#ifndef FILES_H
+#define FILES_H
+
+void files();
+
+#endif
