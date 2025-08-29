7bb405a384dd6f3e7e13960fa38d7225329dc7f2
Dltp <votinovilya174@gmail.com>
2025-08-29 11:50:04 +0500

    Update start.c

--- version_0.1.6/start.c
+++ version_0.1.6/start.c
@@ -14,3 +14,24 @@ void welcome()
     printf("%s\n", thrdln);
     printf("%s\n", endlne);
 }
+
+void LoadScreen()
+{
+    printf(T_BLUE "[Start loading OS]\n" T_RESET);
+    printf(T_GREEN "[OK]: Compiling term.c\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading commands\n" T_RESET);
+    printf(T_GREEN "[OK]: Commands is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command help\n" T_RESET);
+    printf(T_GREEN "[OK]: Command help is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command calc\n" T_RESET);
+    printf(T_GREEN "[OK]: Command calc is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command edit\n" T_RESET);
+    printf(T_GREEN "[OK]: Command edit is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command phnxfetch\n" T_RESET);
+    printf(T_GREEN "[OK]: Command phnxfetch is load\n" T_RESET);
+    printf(T_GREEN "[OK]: Start loading command ext\n" T_RESET);
+    printf(T_GREEN "[OK]: Command ext is load\n");
+    printf(T_GREEN "[OK]: Initialization working all commands\n" T_RESET);
+    printf(T_GREEN "[OK]: Optimization working all ocmmands\n" T_RESET);
+    printf(T_BLUE "[Loading is end!]\n" T_RESET);
+}
