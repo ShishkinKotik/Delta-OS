02c3f219311f3a0e8765d0e6024039430a55c0b0
Dltp <votinovilya174@gmail.com>
2025-09-09 19:08:23 +0500

    Update and rename add.h to files.h

---
+++ version_0.2.4/lib/files.h
@@ -0,0 +1,20 @@
+#ifndef ADD_H
+#define ADD_H
+
+void add();
+
+#endif
+
+#ifndef DEL_H
+#define DEL_H
+
+void del();
+
+#endif
+
+#ifndef EDIT_FILE_H
+#define EDIT_FILE_H
+
+void redact();
+
+#endif
