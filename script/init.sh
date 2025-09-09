9a8f2c90afe285eacca23387295545cd8e208f7e
Dltp <votinovilya174@gmail.com>
2025-09-09 19:15:02 +0500

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,6 +1,6 @@
 gnome-terminal &
 cd Delta-OS/version_0.2.4
 mkdir os
-gcc term.c start.c commands.c files.c -o term
+gcc term.c start.c commands.c files.c -o os/term
 os/term
 echo script is working
