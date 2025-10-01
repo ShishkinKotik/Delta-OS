05421fde85add187c30ccf60400c9c2d06c691fc
Dltp <votinovilya174@gmail.com>
2025-10-01 20:53:44 +0500

    Create simple_commands.h

---
+++ sub-system/lib/simple_commands.h
@@ -0,0 +1,20 @@
+#ifndef SIMPLE_COMMANDS_INTRO_H
+#define SIMPLE_COMMANDS_INTRO_H
+
+void intro();
+
+#endif
+
+#ifndef SIMPLE_COMMANDS_HELP_H
+#define SIMPLE_COMMANDS_HELP_H
+
+void help();
+
+#endif
+
+#ifndef SIMPLE_COMMANDS_FILES_H
+#define SIMPLE_COMMANDS_FILES_H
+
+void files();
+
+#endif
