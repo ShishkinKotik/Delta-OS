ad60c474da197caee31e581ca95273230fcfc480
Dltp <votinovilya174@gmail.com>
2025-10-01 20:55:56 +0500

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,15 +1,15 @@
 echo "cloning repo"
 git clone https://github.com/ShishkinKotik/Delta-OS
 cd ~/Delta-OS/sub-system
-echo "starting OS..."
+echo "✓starting OS..."
 $STATUS_BOOTING
 $STATUS_VERSION
 $STATUS_OS
-echo "status: version=v.0.0.0.9-a $STATUS_VERSION"
-echo "status: booting=true $STATUS_BOOTING"
-echo "status: OS=DeltaOS Linux $STATUS_OS"
+echo "✓status: version=v.0.0.0.9-a $STATUS_VERSION"
+echo "✓status: booting=true $STATUS_BOOTING"
+echo "✓status: OS=DeltaOS Linux $STATUS_OS"
 mkdir os
-gcc term.c start.c commands.c files.c -o os/term
+clang term.c start.c commands.c files.c simple_commands.c -o os/term
 os/term
 chmod +x ~/os/term
-echo "ending os"
+echo "✓ending os"
