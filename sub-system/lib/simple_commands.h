c1e1ec4d33008195aa10c6c116cd2c94ce772d72
Dltp <votinovilya174@gmail.com>
2025-10-03 15:10:47 +0500

    Update simple_commands.h

--- sub-system/lib/simple_commands.h
+++ sub-system/lib/simple_commands.h
@@ -18,3 +18,24 @@ void help();
 void files();
 
 #endif
+
+#ifndef TITLE_OS_H
+#define TITLE_OS_H
+
+void title();
+
+#endif
+
+#ifndef WELCOME_H
+#define WELCOME_H
+
+void welcome();
+
+#endif
+
+#ifndef LOAD_H
+#define LOAD_H
+
+void LoadScreen();
+
+#endif
