05c9eadb258c3580cfe5a43f14b62d8204653acf
Dltp <votinovilya174@gmail.com>
2025-10-11 12:04:13 +0500

    Update simple_comms.c

--- sub-system/simple_comms.c
+++ sub-system/simple_comms.c
@@ -22,22 +22,21 @@ void intro()
 void help()
 {
     printf("[help page]\n");
-    const char frame[128]= T_BLUE   "[================================================================================]" T_RESET;
-    const char frm_2[128] = T_BLUE  "|                                                                                |" T_RESET;
-    const char intr[250] = T_YELLOW "|      1.intro - иснтрукция по работе с ОС                                       |" T_RESET;
-    const char calc[250] = T_YELLOW "|      🖩 2.calc - калькулятор                                                    |" T_RESET;
-    const char edit[250] = T_YELLOW "|      🗒 3.dex - примитивный текстовый редактор                                  |" T_RESET;
-    const char help[250] = T_YELLOW "|      ǂ 4.help - вывод всех команд                                               |" T_RESET;
-    const char ftch[250] = T_YELLOW "|      🖮 5.dlt-fetch - выводит логотип ОС а также информация о компьютере и ос   |" T_RESET;
-    const char exit[250] = T_YELLOW "|      6.quit - выход и завершение работы терминала                               |" T_RESET;
-    const char fils[250] = T_YELLOW "|      🗉 7.lsf - список файлов в помощью которых работает ос                      |" T_RESET;
-    const char add[250] =  T_YELLOW "|      🖹 8.add - добавление с файлов                                              |" T_RESET;
-    const char del[250] =  T_YELLOW "|      🖹 9.del - удаление файлов                                                  |" T_RESET;
-    const char list[250] = T_YELLOW "|      🗉 10.list - список файлов которые вы создали                               |" T_RESET;
-    const char red[250] = T_YELLOW  "|      🖹 11.redact - редактирование файла                                         |" T_RESET;
-    const char dcf[250] = T_YELLOW  "|      🖹 12.display - просмотр содержимого файла                                  |" T_RESET;
-    const char frm_3[128] = T_BLUE  "|                                                                                  |" T_RESET;
-    const char frm_1[128] = T_BLUE  "[==================================================================================]" T_RESET;
+    const char frame[250]= T_BLUE   "[==========================================================================================================]" T_RESET;
+    const char frm_2[250] = T_BLUE  "|                                                                            |                             |" T_RESET;
+    const char intr[250] = T_YELLOW "|      1.intro - инструкция по работе с ОС                                   | 12.gtd - переход в папку.   |" T_RESET;
+    const char calc[250] = T_YELLOW "|      2.calc - калькулятор                                                  | 13.std - вод текущей папки. |" T_RESET;
+    const char edit[250] = T_YELLOW "|      3.dex - примитивный текстовый редактор                                |                             |" T_RESET;
+    const char help[250] = T_YELLOW "|      4.help - вывод всех команд                                            |                             |" T_RESET;
+    const char ftch[250] = T_YELLOW "|      5.dlt-fetch - выводит логотип ОС а также информация о компьютере и ос |                             |" T_RESET;
+    const char exit[250] = T_YELLOW "|      6.quit - выход и завершение работы терминала                          |                             |" T_RESET;
+    const char fils[250] = T_YELLOW "|      7.lsf - список файлов в помощью которых работает ос                   |                             |" T_RESET;
+    const char add[250] =  T_YELLOW "|      8.add - добавление с файлов                                           |                             |" T_RESET;
+    const char del[250] =  T_YELLOW "|      9.del - удаление файлов                                               |                             |" T_RESET;
+    const char list[250] = T_YELLOW "|      10.list - список файлов которые вы создали                            |                             |" T_RESET;
+    const char dcf[250] = T_YELLOW  "|      11.display - просмотр содержимого файла                               |                             |" T_RESET;
+    const char frm_3[250] = T_BLUE  "|                                                                            |                             |" T_RESET;
+    const char frm_1[250] = T_BLUE  "[==========================================================================================================]" T_RESET;
 
     printf("%s\n", frame);
     printf("%s\n", frm_2);
@@ -47,28 +46,36 @@ void help()
     printf("%s\n", help);
     printf("%s\n", ftch);
     printf("%s\n", exit);
+    printf("%s\n", fils);
     printf("%s\n", add);
     printf("%s\n", del);
-    printf("%s\n", red);
+    printf("%s\n", list);
     printf("%s\n", dcf);
-    printf("%s\n", fils);
     printf("%s\n", frm_3);
     printf("%s\n", frm_1);
 }
 
 void files()
 {
-    const char term_dot_c[128] = T_CYAN " [файлы Си]: \n [term.c] \n" T_RESET;
-    const char comm_dot_c[115] = T_CYAN " [commands.c] \n" T_RESET;
-    const char strt_dot_c[115] = T_CYAN " [start.c] \n" T_RESET;
-    const char add_dot_c[115] = T_CYAN " [files.c] \n" T_RESET;
-    const char add_dot_h[115] = T_CYAN " [files.h] \n" T_RESET;
-    const char strt_dot_h[115] = T_CYAN " [start_screen.h]\n" T_RESET;
-    const char comm_dot_h[115] = T_CYAN " [commands.h] \n" T_RESET;
-    const char start_dot_sh[115] = T_GREEN "[bash]: \n [build.sh] \n" T_RESET;
-    const char logo_dot_txt[115] = T_CYAN "[txt]: \n [logo.txt] \n" T_RESET;
-    const char bin_file[115] = T_MAGENTA "[bin]: \n [term.o]\n" T_RESET;
+    const char frame[128] =        T_CYAN    "[===================]\n" T_RESET;
+    const char c_lang[128] =       T_CYAN    "| [C language]      |\n" T_RESET;
+    const char term_dot_c[128] =   T_CYAN    "|  [term.c]         |\n" T_RESET;
+    const char comm_dot_c[115] =   T_CYAN    "|  [commands.c]     |\n" T_RESET;
+    const char strt_dot_c[115] =   T_CYAN    "|  [start.c]        |\n" T_RESET;
+    const char add_dot_c[115] =    T_CYAN    "|  [files.c]        |\n" T_RESET;
+    const char add_dot_h[115] =    T_CYAN    "|  [files.h]        |\n" T_RESET;
+    const char strt_dot_h[115] =   T_CYAN    "|  [start_screen.h] |\n" T_RESET;
+    const char comm_dot_h[115] =   T_CYAN    "|  [commands.h]     |\n" T_RESET;
+    const char bash[115] =         T_GREEN   "| [bash]            |\n" T_RESET;
+    const char start_dot_sh[115] = T_GREEN   "|  [build.sh]       |\n" T_RESET;
+    const char text[115] =         T_YELLOW  "| [txt]             |\n" T_RESET;
+    const char logo_dot_txt[115] = T_YELLOW  "|   [logo.txt]      |\n" T_RESET;
+    const char bin[115] =          T_MAGENTA "| [binary]          |\n" T_RESET;
+    const char bin_file[115] =     T_MAGENTA "|  [term.o]         |\n" T_RESET;
+    const char end_frame[114] =    T_CYAN    "[===================]\n" T_RESET;
 
+    printf("%s", frame);
+    printf("%s", c_lang);
     printf("%s", term_dot_c);
     printf("%s", comm_dot_c);
     printf("%s", comm_dot_h);
@@ -76,9 +83,13 @@ void files()
     printf("%s", add_dot_h);
     printf("%s", strt_dot_h);
     printf("%s", strt_dot_c);
+    printf("%s", bash);
     printf("%s", start_dot_sh);
+    printf("%s", text);
     printf("%s", logo_dot_txt);
+    printf("%s", bin);
     printf("%s", bin_file);
+    printf("%s", end_frame);
 }
 
 
