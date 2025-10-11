9ced73c33c1407d6cac3eecef73a0d99ac03994c
Dltp <votinovilya174@gmail.com>
2025-10-11 12:01:41 +0500

    Update files.c

--- sub-system/files.c
+++ sub-system/files.c
@@ -3,6 +3,17 @@
 #include <stdbool.h>
 #include "lib/commands.h"
 
+#ifdef _WIN32
+    #include <direct.h>
+    #define mkdir(path, mode) _mkdir(path)
+    #define chdir _chdir
+    #define getcwd _getcwd
+#else
+    #include <unistd.h>
+    #include <sys/stat.h>
+    #include <dirent.h>
+#endif
+
 #define T_RED "\033[38;2;255;0;0m"
 #define T_GREEN "\033[38;2;0;255;0m"
 #define T_BLUE "\033[38;2;0;0;255m"
@@ -11,34 +22,33 @@
 #define T_RESET "\033[0m"
 #define T_MAGENTA "\033[0;35m"
 
-#ifndef ADD_H
-#define ADD_H
-
 #define MFNL 256 //maximum file name length
-#define MAX_TOKEN_LENGTH 128
-#define MAX_FOLDER_NAME_LENGTH 128
+#define MAX_TOKEN_LENGTH 1024
+#define MAX_FOLDER_NAME_LENGTH 1024
 #define MAX_CONTENT_LENGTH 1024
 #define MAX_LINES 512
 #define MAX_LINE_LENGTH 1024
 #define MAX_FOLDER_LENGTH 128
 
-struct FileExplorer {
+struct file_explorer {
     /*variables*/
     char fileName[MFNL];
     char token[MAX_TOKEN_LENGTH];
     char line[MAX_LINES];
     char folderName[MAX_FOLDER_LENGTH];
+    char cwd[MAX_FOLDER_NAME_LENGTH];
     FILE * file;
     int isCreated;
     int isDeleted;
+    int isDisplaying;
 };
 
 /*add file */
 void add()
 {
-    struct FileExplorer var = {0};
+    struct file_explorer var;
 
-    printf(T_CYAN "[введите что будем добавлять]: \n" T_RESET);
+    printf(T_CYAN "[введите что будем добавлять(file/dir)]: \n" T_RESET);
     scanf("%s\n", var.token);
     if (strcmp(var.token, "file") == 0) {
         printf(T_CYAN "[🖹 введите имя файла]: \n" T_RESET);
@@ -46,14 +56,30 @@ void add()
         printf(T_CYAN "[файл '%s' создан]\n" T_RESET, var.fileName);
 
         int isCreated = 0;
-        var.file = fopen(var.fileName, "w");
+        var.file = fopen(var.fileName, "a");
         if (var.file == NULL) {
             printf(T_RED "[ошибка создания файла]\n" T_RESET);
-            isCreated = 1;
+            var.isCreated = 0;
         }
         else {
             fclose(var.file);
-            isCreated = 1;
+            var.isCreated = 1;
+        }
+    }
+
+    else if (strcmp(var.token, "dir") == 0) {
+        printf(T_CYAN "[🖹 введите имя папки]: \n" T_RESET);
+        scanf("%s\n", var.folderName);
+        mkdir(var.folderName, 0777);
+        printf(T_CYAN "[папка '%s' создана]\n" T_RESET, var.folderName);
+
+        if (var.file == NULL) {
+            printf(T_RED "[ошибка создания папки]\n" T_RESET);
+            var.isCreated = 1;
+        }
+        else {
+            fclose(var.file);
+            var.isCreated = 0;
         }
     }
 }
@@ -61,37 +87,80 @@ void add()
 /*display content in file */
 void displayFile(const char *fileName)
 {
-    struct FileExplorer var = {0};
+    struct file_explorer var;
 
-    if (var.file == NULL) {
-        printf(T_RED "[ошибка открытия файла для чтения]\n" T_RESET);
-        return;
+    var.file = fopen(var.fileName, "r");
+    printf(T_CYAN "[введите имя файла]: " T_RESET);
+    scanf("%s\n", var.fileName);
+    var.isDisplaying = 0;
+    if (strcmp(var.token, "") == 0) {
+        if (var.file == NULL) {
+            printf(T_RED "[ошибка открытия файла для чтения]\n" T_RESET);
+            return;
+        }
+        else {
+            fclose(var.file);
+            var.isDisplaying = 1;
+        }
     }
 
     while (fgets(var.line, sizeof(var.line), var.file)) {
         printf("%s\n", var.line);
     }
-    fclose(var.file);
 }
 
 /*delete file */
 void del()
 {
-    struct FileExplorer var = {0};
+    struct file_explorer var;
 
-    printf(T_CYAN "[введите что будем удалять]: \n" T_RESET);
+    printf(T_CYAN "[введите что будем удалять(file/dir)]: \n" T_RESET);
     scanf("%s\n", &*var.token);
     if (strcmp(var.token, "file") == 0) {
         printf(T_CYAN "[🖹 введите имя файла]: \n" T_RESET);
         scanf("%s\n", &*var.fileName);
 
-        int isDeleted = 0;
+        var.isDeleted = 0;
         if (remove(var.fileName) == 0) {
             printf(T_GREEN "[файл '%s' удалён!]\n" T_RESET, var.fileName);
+            var.isDeleted = 1;
         }
         else {
             printf(T_RED "[ошибка удаления файла]\n" T_RESET);
+            var.isDeleted = 0;
         }
+    }
+
+    else if (strcmp(var.token, "dir") == 0) {
+        printf(T_CYAN "[🗀 введите имя папки для удаления]: \n" T_RESET);
+        scanf("%s\n", &*var.folderName);
+
+        var.isDeleted = 0;
+        if (remove(var.folderName) == 0) {
+            printf(T_GREEN "[ папка '%s' удалена!]\n" T_RESET, var.folderName);
+            var.isDeleted = 1;
+        }
+        else {
+            printf(T_RED "[ошибка удаления папки]\n" T_RESET);
+            var.isDeleted = 0;
+        }
+    }
+}
+
+/*go to directory */
+void goToDir()
+{
+
+}
+
+void showThisDir()
+{
+    struct file_explorer var = {0};
+    if (getcwd(var.cwd, sizeof(var.cwd)) != NULL) {
+        printf(T_CYAN "[🗀 текущая папка: '%s']: " T_RESET, var.cwd);
+    }
+    else {
+        printf(T_RED "[err]: [удалось определить путь к папке]\n" T_RESET);
     }
 }
 
@@ -100,5 +169,3 @@ void list()
 {
 
 }
-
-#endif
