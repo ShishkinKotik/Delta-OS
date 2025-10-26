f3becc12c38f669799a63bcfc707d2545b03e8e4
Dltp <votinovilya174@gmail.com>
2025-10-26 22:39:38 +0500

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -7,7 +7,7 @@ echo "✓status: version=v.0.0.0.9-a $STATUS_VERSION"
 echo "✓status: booting=true $STATUS_BOOTING"
 echo "✓status: OS=DeltaOS Linux $STATUS_OS"
 mkdir os
-clang term.c commands.c files.c simple_comms.c -o os/term
+gcc term.c commands.c files.c simple_comms.c -o os/term
 os/term
 chmod +x ~/os/term
 echo "✓ending os"
