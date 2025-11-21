9ab0f2cfcb0f56574151ebc62077191267d81f33
DeltaDev <votinovilya174@gmail.com>
2025-11-21 20:21:53 +0500

    Update commands.c

--- sub-system/commands.c
+++ sub-system/commands.c
@@ -24,6 +24,7 @@
 #define MAX_OS_TITLE_LANGTH 128
 #define MAX_HOST_NAME_LENGTH 128
 
+/* structures */
 typedef struct {
     const char *user_name[MAX_USER_NAME_LENGTH];
     const char *os_title[MAX_OS_TITLE_LANGTH];
@@ -33,8 +34,31 @@ typedef struct {
     double disk_memory[32768];
 } UserInfo; 
 
+typedef struct {
+    int number_of_versions[16];
+    const char *phoenix[128];
+    const char *signalmann[128];
+    const char *teemagnat[128];
+    const char *hanz_hanz[128];
+    const char *iwgnig[128];
+    const char *neu_delta[128];
+    const char *deltonium[128];
+    const char *open_dlt_sh[128];    
+} Versions;
+
 #ifndef COMMANDS_H
 #define COMMANDS_H
+
+void your_version()
+{
+    Versions ver;
+    *ver.number_of_versions = 8;
+    const char *installed_ver[128];
+    *installed_ver = "v.0.0.0.11-c OpenDltSh\n";
+    printf(T_CYAN "[installed version dltsh]: %s" T_RESET, *installed_ver);
+    *ver.open_dlt_sh = "v.0.0.0.11-c OpenDltSh\n";
+    printf(T_CYAN "[actualy dltsh version]: %s" T_RESET, *ver.open_dlt_sh);
+}
 
 void editorLogo()
 {
